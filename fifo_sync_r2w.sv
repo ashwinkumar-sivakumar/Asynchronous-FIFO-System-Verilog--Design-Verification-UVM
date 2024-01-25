b7692b4b11f807f1d9769aeb83500e4167d5911f
RegallaMahidhar <150110917+RegallaMahidhar@users.noreply.github.com>
2024-01-25 00:01:47 -0800

    Update and rename fifo_sync_r2w.v to fifo_sync_r2w.sv

---
+++ fifo_sync_r2w.sv
@@ -0,0 +1,19 @@
+// Read pointer to write clock synchronizer
+//
+module sync_r2w
+#(
+  parameter ADDRSIZE = 4
+)
+(
+  input  logic wclk, wrst_n,
+  input  logic [ADDRSIZE:0] rptr,
+  output logic [ADDRSIZE:0] wq2_rptr
+);
+
+  logic [ADDRSIZE:0] wq1_rptr;
+
+  always_ff @(posedge wclk or negedge wrst_n)
+    if (!wrst_n) {wq2_rptr,wq1_rptr} <= 0;
+    else {wq2_rptr,wq1_rptr} <= {wq1_rptr,rptr};
+
+endmodule
