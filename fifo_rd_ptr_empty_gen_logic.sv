4ced4152cd99887779bf1927a29d10b121123d9b
RegallaMahidhar <150110917+RegallaMahidhar@users.noreply.github.com>
2024-01-25 00:02:44 -0800

    Update and rename fifo_rd_ptr_empty_gen_logic.v to fifo_rd_ptr_empty_gen_logic.sv

---
+++ fifo_rd_ptr_empty_gen_logic.sv
@@ -0,0 +1,43 @@
+// Read pointer and empty generation
+//
+module rptr_empty
+#(
+  parameter ADDRSIZE = 4
+)
+(
+  input  logic rinc, rclk, rrst_n,
+  input  logic [ADDRSIZE :0] rq2_wptr,
+  output logic rempty,
+  output logic [ADDRSIZE-1:0] raddr,
+  output logic [ADDRSIZE :0] rptr
+);
+
+  logic [ADDRSIZE:0] rbin;
+  logic [ADDRSIZE:0] rgraynext, rbinnext;
+
+  //-------------------
+  // GRAYSTYLE2 pointer
+  //-------------------
+  always_ff @(posedge rclk or negedge rrst_n)
+    if (!rrst_n)
+      {rbin, rptr} <= '0;
+    else
+      {rbin, rptr} <= {rbinnext, rgraynext};
+
+  // Memory read-address pointer (okay to use binary to address memory)
+  assign raddr = rbin[ADDRSIZE-1:0];
+  assign rbinnext = rbin + (rinc & ~rempty);
+  assign rgraynext = (rbinnext>>1) ^ rbinnext;
+
+  //---------------------------------------------------------------
+  // FIFO empty when the next rptr == synchronized wptr or on reset
+  //---------------------------------------------------------------
+  assign rempty_val = (rgraynext == rq2_wptr);
+
+  always_ff @(posedge rclk or negedge rrst_n)
+    if (!rrst_n)
+      rempty <= 1'b1;
+    else
+      rempty <= rempty_val;
+
+endmodule
