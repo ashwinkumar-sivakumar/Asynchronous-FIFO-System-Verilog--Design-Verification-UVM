9c3702d96494d91d3f35d5cb484e7c05603577f6
ashwinkumar-sivakumar <150288839+ashwinkumar-sivakumar@users.noreply.github.com>
2024-01-26 10:04:43 -0800

    Update fifomem.v
    changed the code from verilog to system verilog removed the vendor dual port ram

--- fifomem.v
+++ fifomem.v
@@ -1,21 +1,25 @@
-module fifomem #(parameter DATASIZE = 8, // Memory data word width
- parameter ADDRSIZE = 4) // Number of mem address bits
- (output [DATASIZE-1:0] rdata,
- input [DATASIZE-1:0] wdata,
- input [ADDRSIZE-1:0] waddr, raddr,
- input wclken, wfull, wclk);
- `ifdef VENDORRAM
- // instantiation of a vendor's dual-port RAM
- vendor_ram mem (.dout(rdata), .din(wdata),
- .waddr(waddr), .raddr(raddr),
- .wclken(wclken),
- .wclken_n(wfull), .clk(wclk));
- `else
- // RTL Verilog memory model
- localparam DEPTH = 1<<ADDRSIZE;
- reg [DATASIZE-1:0] mem [0:DEPTH-1];
- assign rdata = mem[raddr];
- always @(posedge wclk)
- if (wclken && !wfull) mem[waddr] <= wdata;
- `endif
+// Ashwin module 
+module fifomem
+#(
+  parameter DATASIZE = 8, // word width - not finalized 
+  parameter ADDRSIZE = 4  // Nmem address bits  - not finalized
+)
+(
+  input  logic winc, wfull, wclk,
+  input  logic [ADDRSIZE-1:0] waddr, raddr,
+  input  logic [DATASIZE-1:0] wdata,
+  output logic [DATASIZE-1:0] rdata
+);
+
+  
+  localparam DEPTH = 1<<ADDRSIZE;
+
+  logic [DATASIZE-1:0] mem [0:DEPTH-1];
+
+  assign rdata = mem[raddr];
+
+  always_ff @(posedge wclk)
+    if (winc && !wfull)
+      mem[waddr] <= wdata;
+
 endmodule
