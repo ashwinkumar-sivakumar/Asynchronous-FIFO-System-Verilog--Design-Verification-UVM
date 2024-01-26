29c3a61ac771b92a7c0b8468c90050e7ce1e5006
ashwinkumar-sivakumar <150288839+ashwinkumar-sivakumar@users.noreply.github.com>
2024-01-26 10:16:35 -0800

    Update sync_w2r.v

--- sync_w2r.v
+++ sync_w2r.v
@@ -1,9 +1,20 @@
-module sync_w2r #(parameter ADDRSIZE = 4)
- (output reg [ADDRSIZE:0] rq2_wptr,
- input [ADDRSIZE:0] wptr,
- input rclk, rrst_n);
- reg [ADDRSIZE:0] rq1_wptr;
- always @(posedge rclk or negedge rrst_n)
- if (!rrst_n) {rq2_wptr,rq1_wptr} <= 0;
- else {rq2_wptr,rq1_wptr} <= {rq1_wptr,wptr};
+// ashwin module
+module sync_w2r
+#(
+  parameter ADDRSIZE = 4
+)
+(
+  input  logic rclk, rrst_n,
+  input  logic [ADDRSIZE:0] wptr,
+  output logic [ADDRSIZE:0] rq2_wptr
+);
+
+  logic [ADDRSIZE:0] rq1_wptr;
+
+  always_ff @(posedge rclk or negedge rrst_n)
+    if (!rrst_n)
+      {rq2_wptr,rq1_wptr} <= 0;
+    else
+      {rq2_wptr,rq1_wptr} <= {rq1_wptr,wptr};
+
 endmodule
