f66aabf1d632240c4283ddbf44b7401e0407123f
RegallaMahidhar <150110917+RegallaMahidhar@users.noreply.github.com>
2024-02-11 23:31:08 -0800

    Update fifo_top.sv
    Added interface

--- fifo_top.sv
+++ fifo_top.sv
@@ -1,25 +1,20 @@
-module async_fifo1
-#(
-  parameter DSIZE = 8,
-  parameter ASIZE = 8
- )
-(
-  input logic  winc, wclk, wrst_n,//winc write enable signal
-  input logic  rinc, rclk, rrst_n,//rinc read enable signal
-  input logic  [DSIZE-1:0] wdata,
 
-  output logic  [DSIZE-1:0] rdata,
-  output logic wfull,
-  output logic rempty
+module async_fifo1 #(parameter DSIZE = 8, ASIZE = 8) (
+  async_fifo_if.master bus
 );
-
   logic [ASIZE-1:0] waddr, raddr;
   logic [ASIZE:0] wptr, rptr, wq2_rptr, rq2_wptr;
 
-  sync_r2w sync_r2w (.*);
+ /* sync_r2w sync_r2w (.*);
   sync_w2r sync_w2r (.*);
   fifomem #(DSIZE, ASIZE) fifomem (.*);
   rptr_empty #(ASIZE) rptr_empty (.*);
-  wptr_full #(ASIZE) wptr_full (.*);
+  wptr_full #(ASIZE) wptr_full (.*); */
+
+  sync_r2w sync_r2w (bus.wclk,bus.wrst_n,rptr,wq2_rptr);
+  sync_w2r sync_w2r (bus.rclk ,bus.rrst_n,wptr,rq2_wptr);
+  fifomem #(DSIZE, ASIZE) fifomem (bus.winc,bus.wfull,bus.wclk,waddr,raddr,bus.wdata,bus.rdata);
+  rptr_empty #(ASIZE) rptr_empty (bus.rinc,bus.rclk,bus.rrst_n,rq2_wptr,bus.rempty,raddr,rptr);
+  wptr_full #(ASIZE) wptr_full (bus.winc,bus.wclk,bus.wrst_n,wq2_rptr,bus.wfull,waddr,wptr);
 
 endmodule
