50d8ba5c6f8d769e4d515a1e3e3159364def1f4d
ashwinkumar-sivakumar <150288839+ashwinkumar-sivakumar@users.noreply.github.com>
2024-02-11 23:32:38 -0800

    Update fifomem.sv

--- fifomem.sv
+++ fifomem.sv
@@ -1,27 +1,27 @@
-
-// Ashwin module 
 module fifomem
 #(
-  parameter DATASIZE = 8, // word width - not finalized 
-  parameter ADDRSIZE = 4  // Nmem address bits  - not finalized
+  parameter DATASIZE = 8, // Memory data word width
+  parameter ADDRSIZE = 8  // Number of mem address bits
 )
 (
-  input  logic winc, wfull, wclk,
-  input  logic [ADDRSIZE-1:0] waddr, raddr,
-  input  logic [DATASIZE-1:0] wdata,
+  input logic  winc, wfull, wclk,
+  input logic  [ADDRSIZE-1:0] waddr, raddr,
+  input logic  [DATASIZE-1:0] wdata,
   output logic [DATASIZE-1:0] rdata
 );
 
-  
-  localparam DEPTH = 1<<ADDRSIZE;
+  // RTL Verilog memory model
+  localparam DEPTH = 255;//2*addsize
 
   logic [DATASIZE-1:0] mem [0:DEPTH-1];
-always_comb begin
-  rdata <= mem[raddr];
-end
 
-  always_ff @(posedge wclk) begin
+  always_comb begin
+   rdata = mem[raddr];
+  end
+
+  always_ff @(posedge wclk)
     if (winc && !wfull)
       mem[waddr] <= wdata;
-  end
+
 endmodule
+
