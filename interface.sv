d7262b817bbe16f02b84a1ecc195b2c54985078b
RegallaMahidhar <150110917+RegallaMahidhar@users.noreply.github.com>
2024-02-11 23:22:36 -0800

    Create interface.sv

---
+++ interface.sv
@@ -0,0 +1,33 @@
+interface async_fifo_if #(parameter DSIZE = 8, ASIZE = 8);
+  logic [DSIZE-1:0] wdata;
+  logic winc;
+  logic wclk;
+  logic wrst_n;
+  logic [DSIZE-1:0] rdata;
+  logic rinc;
+  logic rclk;
+  logic rrst_n;
+  logic wfull;
+  logic rempty;
+
+  // Define clocking block for write clock
+  /*clocking wr_clk @(posedge wclk);
+    output winc, wrst_n;
+  endclocking
+
+  // Define clocking block for read clock
+  clocking rd_clk @(posedge rclk);
+    output rinc, rrst_n;
+  endclocking */
+
+  modport master (
+    input wclk, wrst_n,
+    output wdata, winc, wfull,
+    input rclk, rrst_n,
+    output rdata, rinc, rempty
+  );
+
+  
+
+endinterface
+  
